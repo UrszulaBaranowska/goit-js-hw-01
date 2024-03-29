78bdfb86e56fe200864db22d9de93904a2c11469
UrszulaBaranowska <114485005+UrszulaBaranowska@users.noreply.github.com>
2024-03-29 13:41:31 +0100

    Task 3 finish to check

--- task-3.js
+++ task-3.js
@@ -1,7 +1,13 @@
 function getElementWidth(content, padding, border) {
-  const numerical = content + 2 * padding + 2 * border;
-  console.log(`numerical`);
+  let contentWidth = parseFloat(content);
+  let paddingWidth = parseFloat(padding) * 2;
+  let borderWidth = parseFloat(border) * 2;
+
+  let totalWidth = contentWidth + paddingWidth + borderWidth;
+
+  return totalWidth;
 }
+
 console.log(getElementWidth("50px", "8px", "4px")); // 74
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
